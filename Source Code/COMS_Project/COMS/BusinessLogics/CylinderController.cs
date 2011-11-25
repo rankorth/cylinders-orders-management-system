122d661ad8bc63bcb0c0f596e4a01f044a46d44e
rogeriss2010 <rogeriss2010@ca5b5b02-42cc-0674-3bfa-cfa28e286f71>
2011-11-25 16:11:08 +0000

    Updated by Roger

--- Source Code/COMS_Project/COMS/BusinessLogics/CylinderController.cs
+++ Source Code/COMS_Project/COMS/BusinessLogics/CylinderController.cs
@@ -9,7 +9,100 @@ using System.Data.Entity;
 
 namespace BusinessLogics
 {
-    class CylinderController
+    public class CylinderController
     {
+        private COMSEntities dbContext = new COMSEntities();
+        public void changeCylinderPriority(Cylinder cylinder)
+        {
+            try
+            {
+                if (null != cylinder && null != dbContext)
+                {
+                    IQueryable<Cylinder> cylinders = dbContext.Cylinders.Where(s => s.cylinderId.Equals(cylinder.cylinderId));
+                    if (null != cylinders)
+                    {
+                        foreach (Cylinder s in cylinders)
+                        {
+                            if (null != s)
+                                s.priority = cylinder.priority;
+                        }
+                    }
+                    dbContext.SaveChanges(System.Data.Objects.SaveOptions.AcceptAllChangesAfterSave);
+                }
+            }
+            catch (Exception ex)
+            {
+                //related to any errors, there may be only database error
+                //always create a meaningful error exception to catch and show up on UI.
+                throw new Exception("Sorry, there is an error occured while updating the cylinder " + cylinder.cylinderId + "'s priority");
+            }
+        }
+
+        public IQueryable<Cylinder> retrieveCylinderList()
+        {
+            try
+            {
+                if (null != dbContext)
+                {
+                    if (null != dbContext.Cylinders)
+                        return dbContext.Cylinders;
+                }
+            }
+            catch (Exception ex)
+            {
+                //related to any errors, there may be only database error
+                //always create a meaningful error exception to catch and show up on UI.
+                throw new Exception("Sorry, there is an error occured while retrieving the cylinder list from the database.");
+            }
+            return null;
+        }
+
+        public void startProduction(String cylinderID)
+        {
+            try
+            {
+                if(null!= cylinderID && null!=dbContext){
+                    IQueryable<Cylinder> cylinders = dbContext.Cylinders.Where(s => s.cylinderId.Equals(cylinderID));
+                    if(null!=cylinders){
+                        foreach (Cylinder s in cylinders)
+                        {
+                            if (null != s)
+                                s.status = 1; //1 means start 
+                        }
+                    }
+                    dbContext.SaveChanges(System.Data.Objects.SaveOptions.AcceptAllChangesAfterSave);
+                }
+            }
+            catch (Exception ex)
+            {
+                //related to any errors, there may be only database error
+                //always create a meaningful error exception to catch and show up on UI.
+                throw new Exception("Sorry, there is an error occured while starting production for the cylinder: " + cylinderID);
+            }
+        }
+
+        public void stopProduction(String cylinderID)
+        {
+            try
+            {
+                if(null!= cylinderID && null!=dbContext){
+                    IQueryable<Cylinder> cylinders = dbContext.Cylinders.Where(s => s.cylinderId.Equals(cylinderID));
+                    if(null!=cylinders){
+                        foreach (Cylinder s in cylinders)
+                        {
+                            if(null!=s)
+                                s.status = 0; //0 means stop 
+                        }
+                    }
+                    dbContext.SaveChanges(System.Data.Objects.SaveOptions.AcceptAllChangesAfterSave);
+                }
+            }
+            catch (Exception ex)
+            {
+                //related to any errors, there may be only database error
+                //always create a meaningful error exception to catch and show up on UI.
+                throw new Exception("Sorry, there is an error occured while stopping production for the cylinder: " + cylinderID);
+            }
+        }
     }
 }
