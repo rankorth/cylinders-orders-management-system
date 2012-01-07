aa232b0f74f04fc80f949158b428cf8d9b55f137
tinkyawoo@gmail.com <tinkyawoo@gmail.com@ca5b5b02-42cc-0674-3bfa-cfa28e286f71>
2012-01-07 03:59:34 +0000

    added new menus

---
+++ Source Code/COMS_Project/COMS/WebUI/Index.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebUI
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Common.BasePage bp = new Common.BasePage();
+            Panel menupanel = (Panel)this.Master.FindControl("module_panel");
+            LinkButton LogoutBtn = (LinkButton)this.Master.FindControl("lnkLogout");
+            LogoutBtn.Visible = false;
+
+            menupanel.Controls.Clear();
+
+            bp.GenerateStartUpMenu(menupanel);
+        }
+
+        protected void lnkLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/Admin/Role.aspx");
+        }
+
+
+    }
+}
