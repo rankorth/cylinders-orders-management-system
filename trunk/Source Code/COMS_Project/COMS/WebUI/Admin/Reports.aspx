630933413546d06c3dd8264a502c447ca32bd3c1
tinkyawoo@gmail.com <tinkyawoo@gmail.com@ca5b5b02-42cc-0674-3bfa-cfa28e286f71>
2012-01-09 15:39:38 +0000

    Report fiel added

---
+++ trunk/Source Code/COMS_Project/COMS/WebUI/Admin/Reports.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true"
+    CodeBehind="Reports.aspx.cs" Inherits="WebUI.Admin.Reports" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ModuleName" runat="server">
+    Reports
+</asp:Content>
+<asp:Content ID="Content5" runat="server" ContentPlaceHolderID="MenuPlaceHolder">
+    <div style="vertical-align:middle;display:block;line-height:35px;">
+    <asp:DropDownList 
+        ID="ddlReports" runat="server" Width="315px" >
+    </asp:DropDownList>
+    &nbsp;
+    <asp:Button ID="btnShowReports" runat="server" Height="25px" Text="Show" 
+        Width="100px" onclick="btnShowReports_Click" />
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="InputPlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="OutputPlaceHolder" runat="server">
+</asp:Content>
+
---
+++ trunk/Source Code/COMS_Project/COMS/WebUI/Admin/Reports.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using BusinessLogics;
+using COMSdbEntity;
+
+namespace WebUI.Admin
+{
+    public partial class Reports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                load_reports_list();
+            }
+        }
+
+        private void load_reports_list()
+        {
+            COMSEntities context = new COMSEntities();
+            IQueryable<Access_Right> reportlist = context.Access_Right.Where(ar => ar.action.Equals("Report"));
+
+            foreach(Access_Right ar in reportlist)
+            {
+                ddlReports.Items.Add(new ListItem(ar.name, "/Reports/"+ ar.module_name));
+            }
+        }
+
+        protected void btnShowReports_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ddlReports.SelectedValue))
+            {
+                Response.Redirect(ddlReports.SelectedValue);
+            }
+        }
+    }
+}
